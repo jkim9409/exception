ddca3451e930af10dea60d31bd6cccc93d4111e6
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2023-04-01 21:35:44 +0900

    daily commit
    daily commit

--- exception/src/main/java/hello/exception/ExceptionApplication.java
+++ exception/src/main/java/hello/exception/ExceptionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExceptionApplication {
-
+	//daily commit
 	public static void main(String[] args) {
 		SpringApplication.run(ExceptionApplication.class, args);
 	}
