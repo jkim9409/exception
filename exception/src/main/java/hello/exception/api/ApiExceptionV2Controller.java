130a59274560c5344df06d5dafe0abff18541409
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2023-04-16 18:35:52 +0900

    Update ApiExceptionV2Controller.java

--- exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -17,6 +18,20 @@ public class ApiExceptionV2Controller {
     public ErrorResult illegalExHandler(IllegalArgumentException e) {
         log.error("[exceptionHandler] ex", e);
         return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부오류");
     }
 
     @GetMapping("/api2/members/{id}")
